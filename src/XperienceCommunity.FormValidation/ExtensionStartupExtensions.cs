0adcb7b5176998cfcf254477456d4abf1f769590
BenQuinlan <ben.quinlan@zeroseven.com.au>
2025-04-22 09:17:19 +1000

    Removed commented out code

--- src/XperienceCommunity.FormValidation/ExtensionStartupExtensions.cs
+++ src/XperienceCommunity.FormValidation/ExtensionStartupExtensions.cs
@@ -8,17 +8,12 @@ namespace XperienceCommunity.FormValidation;
 public static class ExtensionStartupExtensions
 {
     /// <summary>
-    /// Adds page type restrictions extension dependencies
+    /// Adds form validation extension dependencies
     /// </summary>
     /// <param name="serviceCollection">the <see cref="IServiceCollection"/> which will be modified</param>
     /// <returns>Returns this instance of <see cref="IServiceCollection"/>, allowing for further configuration in a fluent manner.</returns>
     public static IServiceCollection AddFormValidationExtensionServices(this IServiceCollection serviceCollection)
     {
-        //var luceneClientService = serviceCollection.FirstOrDefault(x => x.ServiceType == typeof(IFormBuilderConfigurationSerializer));
-        //serviceCollection.Remove(luceneClientService);
-        //serviceCollection.AddKeyedSingleton(typeof(IFormBuilderConfigurationSerializer), "kentico", luceneClientService.ImplementationType);
-        //serviceCollection.AddSingleton<IFormBuilderConfigurationSerializer, CustomFormBuilderConfigurationSerializer>();
-
         var formComponentValidator = serviceCollection.FirstOrDefault(x => x.ServiceType == typeof(IFormComponentValidator));
         serviceCollection.Remove(formComponentValidator);
         serviceCollection.AddKeyedSingleton(typeof(IFormComponentValidator), "kentico", formComponentValidator.ImplementationType);
